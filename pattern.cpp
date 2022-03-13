ca7167c00963ad253d145cb3b240e68448eb77cb
Arun Kumar P <arukha9n@gmail.com>
2022-03-13 09:27:36 -0700

    Added for loop but output is not yet proper

--- pattern.cpp
+++ pattern.cpp
@@ -8,6 +8,13 @@ int main(){
    cout << "Enter the number of row: ";
    cin >> row;
 
+   for(int i{0}; i<row; i++){
+      for(int j{i};j<(2*row-1); j++){
+         cout << "*";
+      }
+      cout << endl;
+   }
+
    
    
    cout << endl;
