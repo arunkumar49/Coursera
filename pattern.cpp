1aed0eff0c96d91d24c9ccf5f5172198ce4c3821
Arun Kumar P <arukha9n@gmail.com>
2022-03-13 09:20:09 -0700

    Just taken number of rows for printing pattern

--- pattern.cpp
+++ pattern.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
 
+using namespace std;
+
+int main(){
+   
+   int row{};
+   cout << "Enter the number of row: ";
+   cin >> row;
+
+   
+   
+   cout << endl;
+   return 0;
+}
